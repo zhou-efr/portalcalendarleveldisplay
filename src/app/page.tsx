a5b7a339c68ff9058ca7961aac4ec96954155f2d
killian.zhou@sepefrei.fr <GHvWrdt5TYeWyMJ>
2023-11-06 01:05:27 +0100

    v01.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,14 +17,16 @@ const monthLength = [
 ]
 
 export default function Home() {
-    const year = 2023;
-    const month = 10;
-    const day = 4;
+    // get current date
+    const date = new Date()
+    const year = date.getFullYear() % 100
+    const month = date.getMonth()
+    const day = date.getDate()
   return (
       <div className={"w-screen h-screen flex flex-col justify-center items-center"}>
         <div className={"flex flex-col w-1/3 h-screen justify-center items-start gap-1"}>
-            <h1 className={"text-[20rem] font-bold leading-[20rem]"}>21</h1>
-            <h2 className={"text-4xl font-bold"}>11/23</h2>
+            <h1 className={"text-[20rem] font-bold leading-[20rem]"}>{day}</h1>
+            <h2 className={"text-4xl font-bold"}>{month + 1}/{year}</h2>
             <div className={"border-t-4 border-black w-full"} />
             <div className={"flex flex-row justify-start items-center w-full gap-1 m-1"}>
                 {
@@ -40,7 +42,7 @@ export default function Home() {
             <div className={"border-t border-black w-full"} />
             <div className={"flex flex-wrap justify-start items-start w-full gap-1 m-1"}>
                 {
-                    getIcons(10, 4).map((v, i) => {
+                    getIcons(month, day).map((v, i) => {
                         return (
                             <Image key={i} className={"w-[16%]"} src={v.image} objectFit={"contain"}  alt={v.name}/>
                         )
